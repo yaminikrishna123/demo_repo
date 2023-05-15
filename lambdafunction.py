e993cb9f45146a6e96374ba72a629d31aea84296
Yamini <yaminikrishna62@gmail.com>
2023-05-15 15:13:49 +0530

    eddited file

--- lambdafunction.py
+++ lambdafunction.py
@@ -21,13 +21,10 @@ def lambda_handler(event, context):
     output={key:value
     for key, values in output_parse.items()
     for value in values} 
-    output_text= output['SAMSUNG-EMS-MIB::emsAlarmText.0']
     output_text= output_text.replace("\n","")
     output_text= output_text.replace('\"','"')
-    output['SAMSUNG-EMS-MIB::emsAlarmText.0'] = output_text
     print(output)
     lambda_client= boto3.client("lambda",region_name="us-east-1")
-    resp= lambda_client.invoke(FunctionName=os.environ.get('function_name'),InvocationType="Event",Payload= json.dumps(output))
     print('successfully invoked Lambda_trigger function')
     file1 = open("/tmp/MyFile.txt", "w")
     file1.write(str(output))
@@ -37,11 +34,9 @@ def lambda_handler(event, context):
     with open('/tmp/MyFile.txt', 'r') as f:
         for i in range(1):
             line = next(f)
-    
-            firehose_client.put_record(DeliveryStreamName=os.environ.get('stream_name'),
                     Record={'Data': line})
         print("sent to firehose")
-    return send_response({"status": "completed"})
+    return send_response({"status": "Yamini"})
     
 
 
